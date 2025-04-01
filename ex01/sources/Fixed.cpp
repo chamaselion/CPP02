fe1f69e1ffa7c6c350db7147e9994a9256ea92b6
Balint Szikora <chamaselion@gmail.com>
2025-04-01 20:04:28 +0200

    Created new functions for the previously made class

---
+++ ex01/sources/Fixed.cpp
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bszikora <bszikora@student.42helbronn.d    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/27 13:07:32 by bszikora          #+#    #+#             */
+/*   Updated: 2025/04/01 20:02:47 by bszikora         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+const int Fixed::bit_number = 8;
+
+Fixed::Fixed() : number(0) {std::cout << "Default constructor called\n";}
+Fixed::Fixed(const Fixed& other)
+{
+	if (this != &other)
+		this->number = other.number;
+	std::cout << "Copy constructor called\n";
+}
+Fixed& Fixed::operator=(const Fixed& other) {
+		this->number = other.number;
+	return (std::cout << "Copy assignment operator called\n", *this);
+}
+Fixed::~Fixed() {std::cout << "Destructor called\n";}
+
+int		Fixed::getRawBits( void ) const
+{
+	std::cout << "getRawBits member function called\n";
+	return this->number;
+}
+
+void	Fixed::setRawBits( int const raw ) {this->number = raw;}
+
+///////////ex01/////////
+Fixed::Fixed(const int toconvert)
+{
+    this->number = toconvert << bit_number;
+    std::cout << "Int constructor called\n";
+}
+
+Fixed::Fixed(const float toconvert)
+{
+    this->number = static_cast<int>(roundf(toconvert * (1 << bit_number)));
+    std::cout << "Float constructor called\n";
+}
+
+std::ostream& operator<<(std::ostream& o, const Fixed& fixed)
+{
+	o << fixed.toFloat();
+	return o;
+}
+
+float	Fixed::toFloat( void ) const {return static_cast<float>(this->number) / (1 << bit_number);}
+
+int		Fixed::toInt( void ) const {return this->number >> bit_number;}
+
