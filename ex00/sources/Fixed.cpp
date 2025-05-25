6c99a398e786f793ea4ab3f2e11e0199e6080fd6
Balint Szikora <chamaselion@gmail.com>
2025-05-25 13:56:15 +0200

    Fixed copy assignment operator and assignment

--- ex00/sources/Fixed.cpp
+++ ex00/sources/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: bszikora <bszikora@student.42helbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 13:07:32 by bszikora          #+#    #+#             */
-/*   Updated: 2025/03/31 13:18:54 by bszikora         ###   ########.fr       */
+/*   Updated: 2025/05/25 13:55:33 by bszikora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,16 @@
 const int Fixed::bit_number = 8;
 
 Fixed::Fixed() : number(0) {std::cout << "Default constructor called\n";}
-Fixed::Fixed(const Fixed& other)
+Fixed::Fixed(const Fixed& other) : number(other.number) {std::cout << "Copy constructor called\n";}
+
+Fixed& Fixed::operator=(const Fixed& other)
 {
 	if (this != &other)
+	{
 		this->number = other.number;
-	std::cout << "Copy constructor called\n";
-}
-Fixed& Fixed::operator=(const Fixed& other) {
-		this->number = other.number;
-	return (std::cout << "Copy assignment operator called\n", *this);
+	}
+	std::cout << "Copy assignment operator called\n";
+	return *this;
 }
 Fixed::~Fixed() {std::cout << "Destructor called\n";}
 
