343117b0ba52d44ed559003e2759f0b8af88c660
Balint Szikora <chamaselion@gmail.com>
2025-06-11 14:03:38 +0200

    Corrected testing code

--- ex00/sources/main.cpp
+++ ex00/sources/main.cpp
@@ -3,99 +3,48 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bszikora <bszikora@student.42heilbronn.    +#+  +:+       +#+        */
+/*   By: bszikora <bszikora@student.42helbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 13:07:28 by bszikora          #+#    #+#             */
-/*   Updated: 2025/05/27 13:19:24 by bszikora         ###   ########.fr       */
+/*   Updated: 2025/06/11 14:03:17 by bszikora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
-
-static int tests_failed = 0;
-
-static void assert_equal(int expected, int actual, const char* test_name)
-{
-    if (expected != actual)
-    {
-        std::cout << "\033[31m" << test_name << " failed: expected "
-                  << expected << " got " << actual << "\033[0m\n";
-        tests_failed++;
-    }
-    else
-    {
-        std::cout << "\033[32m" << test_name << " passed\033[0m\n";
-    }
-}
+#include <iostream>
 
 int main()
 {
-    {
-        Fixed a;
-        assert_equal(0, a.getRawBits(), "Default constructor");
-    }
-    {
-        Fixed a;
-        a.setRawBits(123);
-        assert_equal(123, a.getRawBits(), "set/get positive raw bits");
-        a.setRawBits(-456);
-        assert_equal(-456, a.getRawBits(), "set/get negative raw bits");
-    }
-    {
-        Fixed a;
-        a.setRawBits(2147483647);
-        assert_equal(2147483647, a.getRawBits(), "set/get INT_MAX");
-        a.setRawBits(-2147483648);
-        assert_equal(-2147483648, a.getRawBits(), "set/get INT_MIN");
-    }
-    {
-        Fixed a;
-        a.setRawBits(3.7f);
-        assert_equal(3, a.getRawBits(), "setRawBits positive float literal");
-        a.setRawBits(-3.7);
-        assert_equal(-3, a.getRawBits(), "setRawBits negative double literal");
-    }
-    {
-        Fixed a;
-        a.setRawBits(0.999f);
-        assert_equal(0, a.getRawBits(), "setRawBits positive fraction <1");
-        a.setRawBits(-0.999);
-        assert_equal(0, a.getRawBits(), "setRawBits negative fraction >-1");
-    }
-    {
-        Fixed a;
-        a.setRawBits(1.5f);
-        assert_equal(1, a.getRawBits(), "setRawBits positive fraction .5");
-        a.setRawBits(-1.5);
-        assert_equal(-1, a.getRawBits(), "setRawBits negative fraction .5");
-    }
-    {
-        Fixed a;
-        a.setRawBits(789);
-        Fixed b(a);
-        assert_equal(789, b.getRawBits(), "copy constructor");
-    }
-    {
-        Fixed a;
-        a.setRawBits(42);
-        Fixed b;
-        b = a;
-        assert_equal(42, b.getRawBits(), "assignment operator");
-        b = b;
-        assert_equal(42, b.getRawBits(), "self assignment");
-    }
-    {
-        Fixed a, b, c;
-        a.setRawBits(10);
-        c = b = a;
-        assert_equal(10, b.getRawBits(), "chain assignment b");
-        assert_equal(10, c.getRawBits(), "chain assignment c");
-    }
-    if (tests_failed > 0)
-    {
-        std::cout << tests_failed << " tests failed\n";
-        return 1;
-    }
-    std::cout << "\033[32mAll tests passed\033[0m\n";
-    return 0;
+	// Test Default Constructor
+	{
+		Fixed a;
+		std::cout << "Default constructor: " << a.getRawBits() << std::endl;
+		Fixed b;
+		std::cout << "Default constructor: " << b.getRawBits() << std::endl;
+	}
+
+	// Test Copy Constructor
+	{
+		Fixed a;
+		a.setRawBits(123);
+		Fixed b(a);
+		std::cout << "Copy constructor: " << b.getRawBits() << std::endl;
+		a.setRawBits(-456);
+		Fixed c(a);
+		std::cout << "Copy constructor: " << c.getRawBits() << std::endl;
+	}
+
+	// Test Copy Assignment Operator
+	{
+		Fixed a, b;
+		a.setRawBits(789);
+		b = a;
+		std::cout << "Copy assignment operator: " << b.getRawBits() << std::endl;
+		Fixed c;
+		c.setRawBits(-321);
+		b = c;
+		std::cout << "Copy assignment operator: " << b.getRawBits() << std::endl;
+	}
+	
+	return 0;
 }
